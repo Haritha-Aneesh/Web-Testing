cf7cfc78cd7838d3bc87dfafdf33b81303585fa0
Back-end and front-end developer <71826040+RgTheGreat@users.noreply.github.com>
2021-11-13 21:10:24 +0530

    Update Login_test.java

--- Login_test/Login_test.java
+++ Login_test/Login_test.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class democlass {
+public class Login_test {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\rigve\\Downloads\\chromedriver_win32\\chromedriver.exe");
